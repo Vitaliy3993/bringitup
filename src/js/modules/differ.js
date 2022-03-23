41e4b12be682e9c479c1ec2094e36131e1006830
vitaliy3993 <kadelchukkp@gmail.com>
2022-03-23 15:38:25 +0200

    3

--- src/js/modules/differ.js
+++ src/js/modules/differ.js
@@ -2,23 +2,44 @@ export default class Difference {
     constructor(oldOfficer, newOfficer, items) {
         this.oldOfficer = document.querySelector(oldOfficer);
         this.newOfficer = document.querySelector(newOfficer);
+        this.oldItems = this.oldOfficer.querySelectorAll(items);
+        this.newItems = this.newOfficer.querySelectorAll(items);
         this.items = items;
+        this.oldCounter = 0;
+        this.newCounter = 0;
     }
 
-    hideItems() {
-        this.oldOfficer.querySelectorAll(this.items).forEach((item, i, arr) => {
-            if (i !== arr.length - 1) {
-                item.style.display = 'none';
+    bindTriggers(container, items, counter) {
+        container.querySelector('.plus').addEventListener('click', () => {
+            if (counter !== items.length - 2) {
+                items[counter].style.display = 'flex';
+                items.forEach(item => {
+                    item.classList.add('animated', 'fadeInDown');
+                });
+                counter++;
+            } else {
+                items[counter].style.display = 'flex';
+                items[items.length - 1].remove();
             }
+            console.log(container, counter);
         });
-        this.newOfficer.querySelectorAll(this.items).forEach((item, i, arr) => {
+    }
+
+    hideItems(items) {
+        items.forEach((item, i, arr) => {
             if (i !== arr.length - 1) {
                 item.style.display = 'none';
             }
         });
     }
+
+
 
     init() {
-        this.hideItems();
+        this.bindTriggers(this.oldOfficer, this.oldItems, this.oldCounter);
+        this.bindTriggers(this.newOfficer, this.newItems, this.newCounter);
+
+        this.hideItems(this.newItems);
+        this.hideItems(this.oldItems);
     }
 }
--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -18,7 +18,6 @@ export default class MiniSlider extends Slider {
             this.slides[0].classList.add(this.activeClass);
         }
         
-
         if (this.animate) {
             this.slides[0].querySelector('.card__title').style.opacity = '1';
             this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
